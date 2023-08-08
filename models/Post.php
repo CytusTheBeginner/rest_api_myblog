8b125c8cbe6028e517ba048a513e9e63bb91826e
CytusTheBeginner <hendri.stti@gmail.com>
2023-08-08 21:06:05 +0700

    edit post

--- models/Post.php
+++ models/Post.php
@@ -169,5 +169,31 @@
             printf("Error: %s.\n", $stmt->error);
             return false;
         }
+
+        //delete post
+        public function delete() {
+             //query
+            $query = 'DELETE FROM ' . $this->table . '
+            WHERE
+            id= :id';
+
+            //prepare statement 
+            $stmt = $this->conn->prepare($query);
+
+            //clean data
+            $this->id = htmlspecialchars(strip_tags($this->id));
+
+            //bind data
+            $stmt->bindParam(':id',$this->id);
+
+            //execute
+            if ($stmt->execute()) {
+                return true;
+            }
+
+            //print error if something goes wrong
+            printf("Error: %s.\n", $stmt->error);
+            return false;
+        }
     } 
 ?>
